781b6268c32b6b9892973099232b0026ccb0d586
Luis Bruno <l_brunofidelis@yahoo.com.br>
2018-09-20 10:48:14 -0300

    Criando componente ListaContribuintes
    Utilizado na tela MinhaConta

---
+++ web/src/pages/GeracaoXml/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Botao, CampoTexto, Combo } from '../../components';
+
+
+export default class GeracaoXml extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+
+}
