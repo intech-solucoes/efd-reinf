9f4f0a0508ec8a898a822917dc7719576ee9a28c
Luis Bruno <l_brunofidelis@yahoo.com.br>
2018-09-26 10:48:57 -0300

    Ajuste na tela GeracaoXml

--- web/src/pages/GeracaoXml/index.js
+++ web/src/pages/GeracaoXml/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { handleFieldChange } from '@intechprev/react-lib'; 
 import { Botao, CampoTexto, Combo, Checkbox, Box, Col, Row } from '../../components';
 import ArquivosGerados from './ArquivosGerados';
 
@@ -84,6 +84,20 @@ export default class GeracaoXml extends Component {
         await this.setState({
             erros: this.erros
         });
+    }
+
+    gerar = async () => { 
+        // Validações de campos obrigatórios.
+        await this.limparErros();
+
+        for(var i = 0; i < this.listaCampos.length; i++) {
+            var campo = this.listaCampos[i];
+            campo.validar();
+
+            if(campo.possuiErros) {
+                await this.adicionarErro(campo.erros);
+            }
+        }
     }
 
     onChange = async (checkbox) => { 
@@ -185,6 +199,8 @@ export default class GeracaoXml extends Component {
     }
 
     render() {
+        var opcaoSelecionada = this.state.r1000 || this.state.r1070 || this.state.r2010 || this.state.r2098 || this.state.r2099;
+
         return (
             <div>
                 <Box titulo={"Tipo de Registro"}>
@@ -203,150 +219,152 @@ export default class GeracaoXml extends Component {
                     <Checkbox contexto={this} ref={ (input) => this.listaCampos[4] = input } nome={"r2099"} onChange={() => this.onChangeR2099('r2099')}
                               valor={this.state.r2099} desabilitado={this.state.checkboxR2099Desabilitado} label={"R-2099 - Fechamento de Eventos Periódicos"} />
                 </Box>
-                <br />
 
-                <Box>
-                    {this.state.tipoOperacaoVisivel &&
-                        <Combo contexto={this} label={"Tipo de operação"} ref={ (input) => this.listaCampos[5] = input } 
-                               nome="tipoOperacao" valor={this.state.tipoOperacao} obrigatorio={true} padrao={""}
-                               opcoes={[{nome: "Operação 1", valor: 1}]} />
-                    }
-                    
-                    {this.state.ambienteEnvioVisivel && 
-                        <Combo contexto={this} label={"Ambiente para envio"} ref={ (input) => this.listaCampos[6] = input } 
-                               nome="ambienteEnvio" valor={this.state.ambienteEnvio} obrigatorio={true}
-                               opcoes={[{nome: "Ambiente 1", valor: 1}]} />
-                    }
+                {opcaoSelecionada &&
+                    <Box>
+                        {this.state.tipoOperacaoVisivel &&
+                            <Combo contexto={this} label={"Tipo de operação"} ref={ (input) => this.listaCampos[5] = input } 
+                                   nome="tipoOperacao" valor={this.state.tipoOperacao} obrigatorio={true} padrao={""}
+                                   opcoes={[{nome: "Operação 1", valor: 1}]} />
+                        }
+                        
+                        {this.state.ambienteEnvioVisivel && 
+                            <Combo contexto={this} label={"Ambiente para envio"} ref={ (input) => this.listaCampos[6] = input } 
+                                   nome="ambienteEnvio" valor={this.state.ambienteEnvio} obrigatorio={true}
+                                   opcoes={[{nome: "Ambiente 1", valor: 1}]} />
+                        }
 
-                    {this.state.contribuinteVisivel &&
-                        <CampoTexto contexto={this} ref={ (input) => this.listaCampos[7] = input }
-                                    label={"Contribuinte"} nome={"contribuinte"} tipo={"text"} 
-                                    placeholder={"Contribuinte"} valor={this.state.contribuinte}
-                                    obrigatorio={true} />
-                    }
+                        {this.state.contribuinteVisivel &&
+                            <CampoTexto contexto={this} ref={ (input) => this.listaCampos[7] = input }
+                                        label={"Contribuinte"} nome={"contribuinte"} tipo={"text"} 
+                                        placeholder={"Contribuinte"} valor={this.state.contribuinte}
+                                        obrigatorio={true} />
+                        }
 
-                    {this.state.usuarioResponsavelVisivel &&
-                        <Combo contexto={this} label={"Usuário Responsável"} ref={ (input) => this.listaCampos[8] = input } 
-                               nome="usuarioResponsavel" valor={this.state.usuarioResponsavel} obrigatorio={true}
-                               opcoes={[{nome: "Usuário 1", valor: 1}]} />
-                    }
+                        {this.state.usuarioResponsavelVisivel &&
+                            <Combo contexto={this} label={"Usuário Responsável"} ref={ (input) => this.listaCampos[8] = input } 
+                                   nome="usuarioResponsavel" valor={this.state.usuarioResponsavel} obrigatorio={true}
+                                   opcoes={[{nome: "Usuário 1", valor: 1}]} />
+                        }
 
-                    {this.state.periodoVisivel &&
-                        <Row>
-                            <Col>
-                                <div className="form-group row">
-                                    <div className="col-lg-2 col-md-12 text-lg-right col-form-label">
-                                        <b><label htmlFor="periodoInicial">
-                                            Período *
-                                        </label></b>
-                                    </div>
-
-                                    <div className="col-3">
-                                        <input className="form-control" name="periodoInicial" id="periodoInicial" type="date" />
-                                    </div>
-
-                                    <div className="col-form-label">
-                                        <b><label htmlFor="periodoFinal">
-                                            a
-                                        </label></b>
-                                    </div>
-
-                                    <div className="col-3">
-                                        <input className="form-control" name="periodoFinal" id="periodoFinal" type="date" />
-                                    </div>
-                                </div>
-                            </Col>
-                        </Row>
-                    }
-
-                    {this.state.referenciaVisivel &&
-                        <Row>
-                            <Col>
-                                <div className="form-group row">
-                                    <div className="col-lg-2 col-md-12 text-lg-right col-form-label">
-                                        <b><label htmlFor="referencia">
-                                            Referência *
-                                        </label></b>
-                                    </div>
-
-                                    <div className="col-2">
-                                        <select className="form-control" id="referencia" name="referencia">
-                                            <option>AAAA</option>
-                                        </select>
-                                    </div>
-
-                                    <div className="col-2">
-                                        <select className="form-control" id="referencia" name="referencia">
-                                            <option>MM</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </Col>
-                        </Row>
-                    }
-
-                    {this.state.combosR2099Visiveis && 
-                        <div>
-
-                            <Combo contexto={this} ref={ (input) => this.listaCampos[9] = input } 
-                                   label={"Contratou serviços sujeitos à retenção de contribuição previdenciária?"}
-                                   nome="contratacaoServicos" valor={this.state.contratacaoServicos} obrigatorio={true}
-                                   opcoes={[{nome: "Opção 1", valor: 1}]} />
-
-                            <Combo contexto={this} ref={ (input) => this.listaCampos[10] = input } 
-                                   label={"Prestou serviços sujeitos à retenção de contribuição previdenciária?"} 
-                                   nome="prestacaoServicos" valor={this.state.prestacaoServicos} obrigatorio={true}
-                                   opcoes={[{nome: "Opção 1", valor: 1}]} />
-
-                            <Combo contexto={this} ref={ (input) => this.listaCampos[11] = input } 
-                                   label={"A associação desportiva que mantém equipe de futebol profissional, possui informações sobre recursos recebidos?"}
-                                   nome="associacaoDesportiva" valor={this.state.associacaoDesportiva} obrigatorio={true}
-                                   opcoes={[{nome: "Opção 1", valor: 1}]} />
-
-                            <Combo contexto={this} ref={ (input) => this.listaCampos[12] = input } 
-                                   label={"Possui informações sobre repasses efetuados à associação desportiva que mantém equipe de futebol profissional?"}
-                                   nome="repasseAssociacaoDesportiva" valor={this.state.repasseAssociacaoDesportiva} obrigatorio={true}
-                                   opcoes={[{nome: "Opção 1", valor: 1}]} />
-
-                            <Combo contexto={this} ref={ (input) => this.listaCampos[13] = input } 
-                                   label={"O produtor rural PJ/Agroindústria possui informações de comercialização de produção?"} 
-                                   nome="producaoRural" valor={this.state.producaoRural} obrigatorio={true}
-                                   opcoes={[{nome: "Opção 1", valor: 1}]} />
-
-                            <Combo contexto={this} ref={ (input) => this.listaCampos[14] = input } 
-                                   label={"Possui informações de pagamentos diversos no período de apuração?"}
-                                   nome="pagamentosDiversos" valor={this.state.pagamentosDiversos} obrigatorio={true}
-                                   opcoes={[{nome: "Opção 1", valor: 1}]} />                                                                                                                                                                                                    
-
+                        {this.state.periodoVisivel &&
                             <Row>
                                 <Col>
                                     <div className="form-group row">
                                         <div className="col-lg-2 col-md-12 text-lg-right col-form-label">
-                                            <b><label htmlFor="competencia">
-                                                Competência a partir da qual não houve movimento, cuja situação perdura até a competência atual. *
+                                            <b><label htmlFor="periodoInicial">
+                                                Período *
+                                            </label></b>
+                                        </div>
+
+                                        <div className="col-3">
+                                            <input className="form-control" name="periodoInicial" id="periodoInicial" type="date" value={this.state.periodoInicial} onChange={(e) => handleFieldChange(this.context, e)} />
+                                        </div>
+
+                                        <div className="col-form-label">
+                                            <b><label htmlFor="periodoFinal">
+                                                a
+                                            </label></b>
+                                        </div>
+
+                                        <div className="col-3">
+                                            <input className="form-control" name="periodoFinal" id="periodoFinal" type="date" value={this.state.periodoFinal} onChange={(e) => handleFieldChange(this.context, e)} />
+                                        </div>
+                                    </div>
+                                </Col>
+                            </Row>
+                        }
+
+                        {this.state.referenciaVisivel &&
+                            <Row>
+                                <Col>
+                                    <div className="form-group row">
+                                        <div className="col-lg-2 col-md-12 text-lg-right col-form-label">
+                                            <b><label htmlFor="referencia">
+                                                Referência *
                                             </label></b>
                                         </div>
 
                                         <div className="col-2">
-                                            <select className="form-control" id="competenciaAno" name="competencia">
-                                                <option>AAAA</option>
+                                            <select className="form-control" id="referenciaAno" name="referenciaAno" defaultValue={this.state.referenciaAno} onChange={(e) => handleFieldChange(this.context, e)}>
+                                                <option value="">Selecione uma opção</option>
+                                                <option value="1">AAAA</option>
                                             </select>
                                         </div>
+
                                         <div className="col-2">
-                                            <select className="form-control" id="competenciaMes" name="competencia">
-                                                <option>MM</option>
+                                            <select className="form-control" id="referenciaMes" name="referenciaMes" defaultValue={this.state.referenciaMes} onChange={(e) => handleFieldChange(this.context, e)}>
+                                                <option value="">Selecione uma opção</option>
+                                                <option value="1">MM</option>
                                             </select>
                                         </div>
                                     </div>
                                 </Col>
                             </Row>
+                        }
 
-                        </div>
-                    }
+                        {this.state.combosR2099Visiveis && 
+                            <div>
 
-                    <Botao titulo={"Gerar"} tipo={"primary"} clicar={() => {}} usaLoading={true} />
-                </Box>
-                <br />
+                                <Combo contexto={this} ref={ (input) => this.listaCampos[9] = input } 
+                                       label={"Contratou serviços sujeitos à retenção de contribuição previdenciária?"}
+                                       nome="contratacaoServicos" valor={this.state.contratacaoServicos} obrigatorio={true}
+                                       opcoes={[{nome: "Opção 1", valor: 1}]} />
+
+                                <Combo contexto={this} ref={ (input) => this.listaCampos[10] = input } 
+                                       label={"Prestou serviços sujeitos à retenção de contribuição previdenciária?"} 
+                                       nome="prestacaoServicos" valor={this.state.prestacaoServicos} obrigatorio={true}
+                                       opcoes={[{nome: "Opção 1", valor: 1}]} />
+
+                                <Combo contexto={this} ref={ (input) => this.listaCampos[11] = input } 
+                                       label={"A associação desportiva que mantém equipe de futebol profissional, possui informações sobre recursos recebidos?"}
+                                       nome="associacaoDesportiva" valor={this.state.associacaoDesportiva} obrigatorio={true}
+                                       opcoes={[{nome: "Opção 1", valor: 1}]} />
+
+                                <Combo contexto={this} ref={ (input) => this.listaCampos[12] = input } 
+                                       label={"Possui informações sobre repasses efetuados à associação desportiva que mantém equipe de futebol profissional?"}
+                                       nome="repasseAssociacaoDesportiva" valor={this.state.repasseAssociacaoDesportiva} obrigatorio={true}
+                                       opcoes={[{nome: "Opção 1", valor: 1}]} />
+
+                                <Combo contexto={this} ref={ (input) => this.listaCampos[13] = input } 
+                                       label={"O produtor rural PJ/Agroindústria possui informações de comercialização de produção?"} 
+                                       nome="producaoRural" valor={this.state.producaoRural} obrigatorio={true}
+                                       opcoes={[{nome: "Opção 1", valor: 1}]} />
+
+                                <Combo contexto={this} ref={ (input) => this.listaCampos[14] = input } 
+                                       label={"Possui informações de pagamentos diversos no período de apuração?"}
+                                       nome="pagamentosDiversos" valor={this.state.pagamentosDiversos} obrigatorio={true}
+                                       opcoes={[{nome: "Opção 1", valor: 1}]} />                                                                                                                                                                                                    
+
+                                <Row>
+                                    <Col>
+                                        <div className="form-group row">
+                                            <div className="col-lg-2 col-md-12 text-lg-right col-form-label">
+                                                <b><label htmlFor="competencia">
+                                                    Competência a partir da qual não houve movimento, cuja situação perdura até a competência atual. *
+                                                </label></b>
+                                            </div>
+
+                                            <div className="col-2">
+                                                <select className="form-control" id="competenciaAno" name="competencia">
+                                                    <option>AAAA</option>
+                                                </select>
+                                            </div>
+                                            <div className="col-2">
+                                                <select className="form-control" id="competenciaMes" name="competencia">
+                                                    <option>MM</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </Col>
+                                </Row>
+
+                            </div>
+                        }
+
+                        <Botao titulo={"Gerar"} tipo={"primary"} clicar={this.gerar} usaLoading={true} />
+                    </Box>
+                }
 
                 <ArquivosGerados arquivos={this.state.arquivosGerados} />
             </div>
