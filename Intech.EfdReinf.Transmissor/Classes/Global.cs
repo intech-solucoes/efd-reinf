5bf95756fc460c303aea89a4226c693d800d431c
Rony Moura <ronymmoura@gmail.com>
2018-10-22 16:48:14 -0300

    Criação da base do transmissor

---
+++ Intech.EfdReinf.Transmissor/Classes/Global.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Intech.EfdReinf.Transmissor.Classes
+{
+    public static class Global
+    {
+        public const bool AMBIENTE_TESTES = false;
+        //public const string ENDERECO_BASE_WEBSERVICE_RF = "https://efinanc.receita.fazenda.gov.br/";
+        //public const string ENDERECO_BASE_WEBSERVICE_RF_PREPROD = "https://preprod-efinanc.receita.fazenda.gov.br/";
+        public static X509Certificate2 Certificado { get; set; }
+    }
+}
