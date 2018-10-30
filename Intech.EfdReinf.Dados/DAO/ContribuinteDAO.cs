ac94bb26f008190804b58c54362319cab3cbe5e2
Rony Moura <ronymmoura@gmail.com>
2018-10-30 17:51:24 -0300

    Filtro de lista de contribuintes ativos

--- Intech.EfdReinf.Dados/DAO/ContribuinteDAO.cs
+++ Intech.EfdReinf.Dados/DAO/ContribuinteDAO.cs
@@ -13,60 +13,6 @@ namespace Intech.EfdReinf.Dados.DAO
     public abstract class ContribuinteDAO : BaseDAO<ContribuinteEntidade>
     {
         
-		public virtual ContribuinteEntidade BuscarPorCpfCnpj(string COD_CNPJ_CPF)
-		{
-			try
-			{
-				if(AppSettings.IS_SQL_SERVER_PROVIDER)
-					return Conexao.QuerySingleOrDefault<ContribuinteEntidade>("SELECT * FROM EFD_CONTRIBUINTE WHERE COD_CNPJ_CPF = @COD_CNPJ_CPF", new { COD_CNPJ_CPF });
-				else if(AppSettings.IS_ORACLE_PROVIDER)
-					return Conexao.QuerySingleOrDefault<ContribuinteEntidade>("SELECT * FROM EFD_CONTRIBUINTE WHERE COD_CNPJ_CPF=:COD_CNPJ_CPF", new { COD_CNPJ_CPF });
-				else
-					throw new Exception("Provider não suportado!");
-			}
-			finally
-			{
-				Conexao.Close();
-			}
-		}
-
-		public virtual ContribuinteEntidade BuscarPorCpfCnpjOidUsuario(string COD_CNPJ_CPF, decimal OID_USUARIO)
-		{
-			try
-			{
-				if(AppSettings.IS_SQL_SERVER_PROVIDER)
-					return Conexao.QuerySingleOrDefault<ContribuinteEntidade>("SELECT * FROM EFD_CONTRIBUINTE INNER JOIN EFD_USUARIO_CONTRIBUINTE ON EFD_USUARIO_CONTRIBUINTE.OID_CONTRIBUINTE = EFD_CONTRIBUINTE.OID_CONTRIBUINTE WHERE EFD_CONTRIBUINTE.COD_CNPJ_CPF = @COD_CNPJ_CPF   AND EFD_USUARIO_CONTRIBUINTE.OID_USUARIO = @OID_USUARIO", new { COD_CNPJ_CPF, OID_USUARIO });
-				else if(AppSettings.IS_ORACLE_PROVIDER)
-					return Conexao.QuerySingleOrDefault<ContribuinteEntidade>("SELECT * FROM EFD_CONTRIBUINTE INNER  JOIN EFD_USUARIO_CONTRIBUINTE  ON EFD_USUARIO_CONTRIBUINTE.OID_CONTRIBUINTE=EFD_CONTRIBUINTE.OID_CONTRIBUINTE WHERE EFD_CONTRIBUINTE.COD_CNPJ_CPF=:COD_CNPJ_CPF AND EFD_USUARIO_CONTRIBUINTE.OID_USUARIO=:OID_USUARIO", new { COD_CNPJ_CPF, OID_USUARIO });
-				else
-					throw new Exception("Provider não suportado!");
-			}
-			finally
-			{
-				Conexao.Close();
-			}
-		}
-
-		public virtual IEnumerable<ContribuinteEntidade> BuscarPorOidUsuario(decimal OID_USUARIO)
-		{
-			try
-			{
-				if(AppSettings.IS_SQL_SERVER_PROVIDER)
-					return Conexao.Query<ContribuinteEntidade>("SELECT * FROM EFD_CONTRIBUINTE INNER JOIN EFD_USUARIO_CONTRIBUINTE ON EFD_USUARIO_CONTRIBUINTE.OID_CONTRIBUINTE = EFD_CONTRIBUINTE.OID_CONTRIBUINTE WHERE EFD_USUARIO_CONTRIBUINTE.OID_USUARIO = @OID_USUARIO", new { OID_USUARIO });
-				else if(AppSettings.IS_ORACLE_PROVIDER)
-					return Conexao.Query<ContribuinteEntidade>("SELECT * FROM EFD_CONTRIBUINTE INNER  JOIN EFD_USUARIO_CONTRIBUINTE  ON EFD_USUARIO_CONTRIBUINTE.OID_CONTRIBUINTE=EFD_CONTRIBUINTE.OID_CONTRIBUINTE WHERE EFD_USUARIO_CONTRIBUINTE.OID_USUARIO=:OID_USUARIO", new { OID_USUARIO });
-				else
-					throw new Exception("Provider não suportado!");
-			}
-			finally
-			{
-				Conexao.Close();
-			}
-		}
-
-    }
-}
-
 		public virtual IEnumerable<ContribuinteEntidade> BuscarAtivosPorOidUsuario(decimal OID_USUARIO)
 		{
 			try
@@ -134,3 +80,6 @@ namespace Intech.EfdReinf.Dados.DAO
 				Conexao.Close();
 			}
 		}
+
+    }
+}
